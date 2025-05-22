7afdc36472e6b74b0290fb3ae47e1701f7fc7943
Nishant Kumar <nkhappy1994@gmail.com>
2025-05-23 01:17:31 +0530

    Update urls.js

--- user-data/urls.js
+++ user-data/urls.js
@@ -2,7 +2,7 @@ const githubUsername = "123nishant";
 const mediumUsername = "nishantkumar94";
 
 const createMediumURL = (username) => `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${username}`;
-const createGitConnectedURL = (username) => `https://gitconnected.com/v1/portfolio/${username}/trial_portfolio`;
+const createGitConnectedURL = (username) => `https://gitconnected.com/v1/portfolio/${username}`;
 const gitRepos = (username) => `https://pinned.berrysauce.dev/get/${username}`;
 
 export const URLs = {
