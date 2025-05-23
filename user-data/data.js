ae5fc7bedb1dcda53a096e7eaf24a5c8375edaea
Nishant Kumar <nkhappy1994@gmail.com>
2025-05-24 00:59:33 +0530

    Update data.js

--- user-data/data.js
+++ user-data/data.js
@@ -92,7 +92,7 @@ export const bio = [
       label: "Links",
       data: [
           {
-            text: "Linkedin",
+            text: "/icons/linkedin.png",
             link: "https://www.linkedin.com/in/nishant-kumar-350043a5/",
             icon: "/icons/linkedin.png",
           },
